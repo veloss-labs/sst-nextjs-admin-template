f264f6e1d3f6667cd65fc844ccd79197d723cbd8
OhMinSeop <mins5190@gmail.com>
2023-02-11 22:59:30 +0900

    add: 회원가입 페이지 추가

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -2,26 +2,26 @@ import Link from 'next/link';
 import React from 'react';
 import Square3Stack3DIcon from '@heroicons/react/24/outline/Square3Stack3DIcon';
 import AuthLayout from '~/components/auth/AuthLayout';
-import LoginForm from '~/components/auth/LoginForm';
+import SignupForm from '~/components/auth/SignupForm';
 
-export default function SignInPage() {
+export default function SignupPage() {
   return (
     <div className="auth-form">
       <div>
         <Square3Stack3DIcon className="mx-auto h-6 w-6" />
-        <h1 className="title">Welcome back</h1>
+        <h1 className="title">Create an account</h1>
         <p className="description">
-          계정에 로그인하려면 이메일, 비밀번호를 입력하세요.
+          계정을 만들려면 아래에 정보들을 입력하세요.
         </p>
       </div>
-      <LoginForm />
+      <SignupForm />
       <p className="footer">
-        <Link href="/auth/signup">계정이 없으신가요?&nbsp;회원가입</Link>
+        <Link href="/auth/signin">이미 계정이 있으신가요?&nbsp;로그인</Link>
       </p>
     </div>
   );
 }
 
-SignInPage.getLayout = function GetLayout(page: React.ReactNode) {
+SignupPage.getLayout = function GetLayout(page: React.ReactNode) {
   return <AuthLayout>{page}</AuthLayout>;
 };
